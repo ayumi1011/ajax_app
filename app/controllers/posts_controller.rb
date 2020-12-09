93a8bc32d75a431e55a3c022de3f4c587d409973
ayumi1011 <75243835+ayumi1011@users.noreply.github.com>
2020-12-09 19:13:15 +0900

    Revert "既読機能のサーバーサイド実装"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,15 +7,4 @@ class PostsController < ApplicationController
   def crsate
     Post.creste(content: params[:content])
   end
-  def checked
-    post = Post.find(params[:id])
-    if post.checked 
-      post.update(checked: false)
-    else
-      post.update(checked: true)
-    end
-
-    item = Post.find(params[:id])
-    render json: { post: item }
-  end
 end
